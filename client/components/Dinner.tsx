c5e050a5c7aba2efb5d081c6504d3bf3f4204d1c
Darragh <155495130+Darragh-Carraher@users.noreply.github.com>
2024-04-10 16:26:15 +1200

    remove tailwind

--- client/components/Dinner.tsx
+++ client/components/Dinner.tsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import Jigsaw from './GamePopups/Jigsaw'
 
 export default function Dinner() {
-  const [jigsaw, setJigsaw] = useState(true)
-  return <>{jigsaw && <Jigsaw setJigsaw={setJigsaw} />}Dinner</>
+  const [jigsaw, setJigsaw] = useState(false)
+  return (
+    <>
+      <h1>Dinner</h1>
+      {jigsaw && <Jigsaw setJigsaw={setJigsaw} />}
+    </>
+  )
 }
