3362a2fae13ad206c1ac3d367894de7e86e5cb91
Haruka Ogino <harukariq@gmail.com>
2024-04-11 09:46:08 +1200

    add CSS for game items

--- client/components/GamePopups/Matching.tsx
+++ client/components/GamePopups/Matching.tsx
@@ -1,3 +1,5 @@
+import '../../styles/matching.css'
+
 interface Props {
   setMatching: React.Dispatch<React.SetStateAction<boolean>>
 }
@@ -7,6 +9,15 @@ export default function Matching({ setMatching }: Props) {
     <>
       <h1>Matching</h1>
       <button onClick={() => setMatching(false)}>Close</button>
+      <div className="matching">
+        <div className="item">
+          <p>small fork</p>
+          <button>{'>'}</button>
+        </div>
+        <div className="item">
+          <p>BIG fork</p>
+        </div>
+      </div>
     </>
   )
 }
