3bfbceee7527ca41963f0fafd654525e911c5a23
Darragh <155495130+Darragh-Carraher@users.noreply.github.com>
2024-04-10 17:00:31 +1200

    add clock component

--- client/components/Dinner.tsx
+++ client/components/Dinner.tsx
@@ -3,12 +3,14 @@ import Jigsaw from './GamePopups/Jigsaw'
 import CombinationLock from './GamePopups/CombinationLock'
 import Matching from './GamePopups/Matching'
 import MatchingExample from './GamePopups/MatchingExample'
+import Clock from './GamePopups/CombinationLock copy'
 
 export default function Dinner() {
   const [jigsaw, setJigsaw] = useState(false)
   const [lock, setLock] = useState(false)
   const [matching, setMatching] = useState(false)
   const [matchingE, setMatchingE] = useState(false)
+  const [clock, setClock] = useState(true)
 
   return (
     <>
@@ -17,6 +19,7 @@ export default function Dinner() {
       {lock && <CombinationLock setLock={setLock} />}
       {matching && <Matching setMatching={setMatching} />}
       {matchingE && <MatchingExample setMatchingE={setMatchingE} />}
+      {clock && <Clock setClock={setClock} />}
     </>
   )
 }
