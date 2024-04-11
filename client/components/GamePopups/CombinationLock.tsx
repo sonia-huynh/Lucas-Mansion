ee3e56da426489ed4add6fedb4118fd12b298f69
Sonia <155363911+sonia-huynh@users.noreply.github.com>
2024-04-12 11:01:39 +1200

    css for lock game complete

--- client/components/GamePopups/CombinationLock.tsx
+++ client/components/GamePopups/CombinationLock.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../../styles/lock.css'
 import '../../styles/popup.css'
+import '../../../public/lock-closeup.png'
 
 interface Props {
   setLockNum: React.Dispatch<React.SetStateAction<boolean>>
@@ -25,108 +26,111 @@ export default function CombinationLock({ setLockNum }: Props) {
 
   return (
     <>
-      <h1>Combination Lock</h1>
-      <div className="box">
-        <div className="pin">
-          <button
-            onClick={() => {
-              setPin1(pin1 + 1)
-              if (pin1 > 8) {
-                setPin1(0)
-              }
-            }}
-          >
-            ▲
-          </button>
-          <p>{pin1}</p>
-          <button
-            onClick={() => {
-              setPin1(pin1 - 1)
-              if (pin1 < 1) {
-                setPin1(9)
-              }
-            }}
-          >
-            ▼
-          </button>
-        </div>
-        <div className="pin">
-          <button
-            onClick={() => {
-              setPin2(pin2 + 1)
-              if (pin2 > 8) {
-                setPin2(0)
-              }
-            }}
-          >
-            ▲
-          </button>
-          <p>{pin2}</p>
-          <button
-            onClick={() => {
-              setPin2(pin2 - 1)
-              if (pin2 < 1) {
-                setPin2(9)
-              }
-            }}
-          >
-            ▼
-          </button>
-        </div>
-        <div className="pin">
-          <button
-            onClick={() => {
-              setPin3(pin3 + 1)
-              if (pin3 > 8) {
-                setPin3(0)
-              }
-            }}
-          >
-            ▲
-          </button>
-          <p>{pin3}</p>
-          <button
-            onClick={() => {
-              setPin3(pin3 - 1)
-              if (pin3 < 1) {
-                setPin3(9)
-              }
-            }}
-          >
-            ▼
-          </button>
-        </div>
-        <div className="pin">
-          <button
-            onClick={() => {
-              setPin4(pin4 + 1)
-              if (pin4 > 8) {
-                setPin4(0)
-              }
-            }}
-          >
-            ▲
-          </button>
-          <p>{pin4}</p>
-          <button
-            onClick={() => {
-              setPin4(pin4 - 1)
-              if (pin4 < 1) {
-                setPin4(9)
-              }
-            }}
-          >
-            ▼
+      {/* <h1>Combination Lock</h1> */}
+      <div className="bg">
+        <div className="box">
+          <div className="pin">
+            <button
+              onClick={() => {
+                setPin1(pin1 + 1)
+                if (pin1 > 8) {
+                  setPin1(0)
+                }
+              }}
+            >
+              ▲
+            </button>
+            <p>{pin1}</p>
+            <button
+              onClick={() => {
+                setPin1(pin1 - 1)
+                if (pin1 < 1) {
+                  setPin1(9)
+                }
+              }}
+            >
+              ▼
+            </button>
+          </div>
+          <div className="pin">
+            <button
+              onClick={() => {
+                setPin2(pin2 + 1)
+                if (pin2 > 8) {
+                  setPin2(0)
+                }
+              }}
+            >
+              ▲
+            </button>
+            <p>{pin2}</p>
+            <button
+              onClick={() => {
+                setPin2(pin2 - 1)
+                if (pin2 < 1) {
+                  setPin2(9)
+                }
+              }}
+            >
+              ▼
+            </button>
+          </div>
+          <div className="pin">
+            <button
+              onClick={() => {
+                setPin3(pin3 + 1)
+                if (pin3 > 8) {
+                  setPin3(0)
+                }
+              }}
+            >
+              ▲
+            </button>
+            <p>{pin3}</p>
+            <button
+              onClick={() => {
+                setPin3(pin3 - 1)
+                if (pin3 < 1) {
+                  setPin3(9)
+                }
+              }}
+            >
+              ▼
+            </button>
+          </div>
+          <div className="pin">
+            <button
+              onClick={() => {
+                setPin4(pin4 + 1)
+                if (pin4 > 8) {
+                  setPin4(0)
+                }
+              }}
+            >
+              ▲
+            </button>
+            <p>{pin4}</p>
+            <button
+              onClick={() => {
+                setPin4(pin4 - 1)
+                if (pin4 < 1) {
+                  setPin4(9)
+                }
+              }}
+            >
+              ▼
+            </button>
+          </div>
+          <div className="submitdiv"></div>
+          <br />
+          <button className="submit" onClick={handleSubmit}>
+            x
           </button>
         </div>
       </div>
-      <br />
-      <div className="pin">
-        <button onClick={handleSubmit}>Submit</button>
-        <button className="closeButton" onClick={() => setLockNum(false)}>
-          x
-        </button>
-      </div>
+      <button className="closeButton" onClick={() => setLockNum(false)}>
+        x
+      </button>
     </>
   )
 }
