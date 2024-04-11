432fdda9cd02c0586c074ec98804c13eda93279f
Prashniel Kumar <prashniel@gmail.com>
2024-04-11 16:38:14 +1200

    maze completed

--- client/components/Maze.tsx
+++ client/components/Maze.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react"
 import pathing from "../data/Maze.json"
+import {useNavigate} from "react-router-dom"
 
 export default function Maze() {
-  console.log(pathing[22].forward)
+  const [position, setPosition] = useState(0)
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if (position === 100){
+      navigate("/congrats")
+    }
+  },[position])
 
-  return <>Maze</>
+  if (pathing){
+    return (<>
+    <h1>{pathing[position].point}</h1>
+    {pathing[position].left && <button onClick={()=>setPosition(pathing[position].left)}>left</button>}
+    {pathing[position].forward &&<button onClick={()=>setPosition(pathing[position].forward)}>forward</button>}
+    {pathing[position].right &&<button onClick={()=>setPosition(pathing[position].right)}>right</button>}
+    {pathing[position].back &&<button onClick={()=>setPosition(pathing[position].back)}>back</button>}
+  </>)
+    }
 }
