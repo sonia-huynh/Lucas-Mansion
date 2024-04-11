c10c18edaac0335e4be08c58a21f7bf956d6de04
Haruka Ogino <harukariq@gmail.com>
2024-04-11 14:13:52 +1200

    create divs

--- client/components/GamePopups/Matching.tsx
+++ client/components/GamePopups/Matching.tsx
@@ -38,31 +38,33 @@ export default function Matching({ setMatching, win, setWin }: Props) {
   }
 
   return (
-    <>
-      <h1>Matching</h1>
-      <button onClick={() => setMatching(false)}>Close</button>
-      <div className="matching">
-        {arr.map((item, index) => (
-          <div key={index} className="item">
-            <p>{item}</p>
-            {index === 0 ? (
-              <button onClick={() => handleClick(index, 1)}>{'>'}</button>
-            ) : index === arr.length - 1 ? (
-              <button onClick={() => handleClick(index, -1)}>{'<'}</button>
-            ) : (
-              <div>
-                <button onClick={() => handleClick(index, -1)}>{'<'}</button>
+    <div className="popup-overlay">
+      <div>
+        <h1>Matching</h1>
+        <button onClick={() => setMatching(false)}>Close</button>
+        <div className="matching">
+          {arr.map((item, index) => (
+            <div key={index} className="item">
+              <p>{item}</p>
+              {index === 0 ? (
                 <button onClick={() => handleClick(index, 1)}>{'>'}</button>
-              </div>
-            )}
-          </div>
-        ))}
+              ) : index === arr.length - 1 ? (
+                <button onClick={() => handleClick(index, -1)}>{'<'}</button>
+              ) : (
+                <div>
+                  <button onClick={() => handleClick(index, -1)}>{'<'}</button>
+                  <button onClick={() => handleClick(index, 1)}>{'>'}</button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+        {win ? (
+          <p>go get the key</p>
+        ) : (
+          <button onClick={() => handleCheck()}>Check</button>
+        )}
       </div>
-      {win ? (
-        <p>go get the key</p>
-      ) : (
-        <button onClick={() => handleCheck()}>Check</button>
-      )}
-    </>
+    </div>
   )
 }
