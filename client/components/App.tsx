5e57143b420888b069370b8d1d0affe9034867d8
Prashniel Kumar <prashniel@gmail.com>
2024-04-15 11:55:35 +1200

    made foyer in the wrong branch... oops

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,6 +1,20 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 function App() {
+  const location = useLocation()
+  useEffect(() => {
+    // RegEx will break path into just the words
+    const titleWords: RegExpMatchArray | null =
+      location.pathname.match(/[a-z]+/gi)
+    const newTitle: string | undefined = titleWords
+      ?.map((ele) => {
+        return ele.charAt(0).toUpperCase() + ele.slice(1)
+      })
+      .join(' ')
+    document.title = newTitle ? `Lucas' Mansion - ${newTitle}` : "Lucas' Mansion - Dinner"
+  }, [location])
+
   return (
     <>
       <div className="app">
--- client/components/Foyer.tsx
+++ client/components/Foyer.tsx
@@ -1,14 +1,23 @@
 import { useEffect } from 'react'
 import '../../public/foyer-images/foyer.png'
 import '../styles/foyer.css'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 export default function Foyer() {
+  const navigate = useNavigate()
   useEffect(()=>{
     document.body.style.backgroundImage = "url('/foyer-images/foyer.png')"
   })
-  return (<></>
-    // <div className="foyer">
-    //   <img src="/foyer-images/foyer.png" alt="foyer-background" />
-    // </div>
+  return (<>
+  <div className="foyerdoordiv">
+  <button className="foyerdoor" onClick={() => navigate("../maze")}>
+    <img
+      className='foyerdoor'
+      src="/foyer-images/foyer-door.png"
+      alt="foyer door"
+      />
+  </button>
+</div>
+      </>
   )
 }
