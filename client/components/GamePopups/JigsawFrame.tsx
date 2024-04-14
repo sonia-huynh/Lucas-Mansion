deb5f3f26b13ea7ecc7b7f464a0805900adc974a
Haruka Ogino <harukariq@gmail.com>
2024-04-15 10:44:36 +1200

    conditionally render frame/game complete

--- client/components/GamePopups/JigsawFrame.tsx
+++ client/components/GamePopups/JigsawFrame.tsx
@@ -1,3 +1,12 @@
 export default function JigsawFrame() {
-  return <p>hey</p>
+  return (
+    <div style={{ margin: '115px' }}>
+      <img
+        src="/lucas-paper-closeup.png"
+        alt="maze-map"
+        style={{ height: '500px' }}
+      />
+      <p>You may be missing some things...</p>
+    </div>
+  )
 }
