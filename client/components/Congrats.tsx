b5d05de3dbb85ce979f34c9d87ca9c40df1071bc
Darragh <155495130+Darragh-Carraher@users.noreply.github.com>
2024-04-14 16:16:10 +1200

    add audio

--- client/components/Congrats.tsx
+++ client/components/Congrats.tsx
@@ -1,17 +1,17 @@
 export default function Congrats() {
   return (
     <>
-      <audio controls src={`../../public/audio/long-unlock.mp3`} />
-      <audio controls src={`../../public/audio/bell.mp3`} />
-      <audio
-        controls
-        src={`../../public/audio/lock-36695
-      .mp3`}
-      />
-      <audio controls src={`../../public/audio/combination-unlock.mp3`} />
-      <audio controls src={`../../public/audio/latch-unlock.mp3`} />
-      <audio controls src={`../../public/audio/unlock-padlock.mp3`} />
-      <audio controls src={`../../public/audio/stretch-tape.mp3`} />
+      <audio controls src={`audio/long-unlock.mp3`} />
+      <audio controls src={`audio/bell.mp3`} />
+      <audio controls src={`audio/lock-36695.mp3`} />
+      <audio controls src={`audio/combination-lock.mp3`} />
+      <audio controls src={`audio/latch-unlock.mp3`} />
+      <audio controls src={`audio/unlock-padlock.mp3`} />
+      <audio controls src={`audio/stretch-tape.mp3`} />
+      <audio controls src={`audio/running-in-the-woods.mp3`} />
+      <audio controls src={`audio/running-soundscape.mp3`} />
+      <audio controls src={`audio/night-ambience.mp3`} />
+      {/* <audio controls src={`audio/stretch-tape.mp3`} /> */}
     </>
   )
 }
