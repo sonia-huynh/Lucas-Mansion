664f86fde892a51986852cc5bc0606237ec65060
Prashniel Kumar <prashniel@gmail.com>
2024-04-14 15:33:10 +1200

    Added maze images

--- client/components/Maze.tsx
+++ client/components/Maze.tsx
@@ -13,11 +13,13 @@ export default function Maze() {
 
   if (pathing){
     return (<>
-    <h1>{pathing[position].point}</h1>
-    {pathing[position].left && <button onClick={()=>setPosition(pathing[position].left)}>left</button>}
-    {pathing[position].forward &&<button onClick={()=>setPosition(pathing[position].forward)}>forward</button>}
-    {pathing[position].right &&<button onClick={()=>setPosition(pathing[position].right)}>right</button>}
-    {pathing[position].back &&<button onClick={()=>setPosition(pathing[position].back)}>back</button>}
+    <img src={`/maze/${pathing[position].image}.png`} alt={`maze orientation ${pathing[position].point}`}/>
+    {pathing[position].left && <button onClick={()=>setPosition(pathing[position].left as number)}>left</button>}
+    {pathing[position].forward &&<button onClick={()=>setPosition(pathing[position].forward as number)}>forward</button>}
+    {pathing[position].right &&<button onClick={()=>setPosition(pathing[position].right as number)}>right</button>}
+    {pathing[position].back 
+    ? <button onClick={()=>setPosition(pathing[position].back as number)}>back</button> 
+    : pathing[position].back === 0 && <button onClick={()=>setPosition(pathing[position].back as number)}>back</button>}
   </>)
     }
 }
