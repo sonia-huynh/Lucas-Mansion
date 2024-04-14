fb9b234bd4959bfe403a0daedb0a871c87e60899
Darragh <155495130+Darragh-Carraher@users.noreply.github.com>
2024-04-14 15:23:41 +1200

    add sound effects

--- client/components/Congrats.tsx
+++ client/components/Congrats.tsx
@@ -1,3 +1,17 @@
 export default function Congrats() {
-  return <>Congrats</>
+  return (
+    <>
+      <audio controls src={`../../public/audio/long-unlock.mp3`} />
+      <audio controls src={`../../public/audio/bell.mp3`} />
+      <audio
+        controls
+        src={`../../public/audio/lock-36695
+      .mp3`}
+      />
+      <audio controls src={`../../public/audio/combination-unlock.mp3`} />
+      <audio controls src={`../../public/audio/latch-unlock.mp3`} />
+      <audio controls src={`../../public/audio/unlock-padlock.mp3`} />
+      <audio controls src={`../../public/audio/stretch-tape.mp3`} />
+    </>
+  )
 }
