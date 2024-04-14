2c73e57d353856729e39ef7fa60a717272e5c87e
Prashniel Kumar <prashniel@gmail.com>
2024-04-15 07:56:34 +1200

    database setup

---
+++ server/db/leaderbaord.ts
@@ -0,0 +1,10 @@
+import db from './connection.ts'
+import { Fruit, FruitData } from '../../models/fruit.ts'
+
+export async function getAllScores() {
+  return await db('fruit').select()
+}
+
+export async function getFruitById(name: string, time: string) {
+  return await db('leaderboard').insert{ name, time }
+}
