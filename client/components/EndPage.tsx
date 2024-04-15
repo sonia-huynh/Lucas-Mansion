a55fe26ec17526eb6a34088718c4ff7a54ecea5b
Haruka Ogino <harukariq@gmail.com>
2024-04-16 10:26:52 +1200

    align and size end page items

--- client/components/EndPage.tsx
+++ client/components/EndPage.tsx
@@ -1,8 +1,13 @@
 import { useNavigate } from 'react-router-dom'
 import Congrats from './Congrats'
+import { useEffect } from 'react'
 
 export default function EndPage() {
   const navigate = useNavigate()
+
+  useEffect(() => {
+    document.body.style.backgroundImage = "url('/end-page/end-page.png')"
+  })
 
   function handleGoCongrats() {
     navigate('/congrats')
@@ -10,8 +15,36 @@ export default function EndPage() {
   return (
     <>
       <div>
-        <p>end page</p>
-        <button onClick={handleGoCongrats}>go to congrats</button>
+        <img
+          src="/end-page/game-cleared.png"
+          alt="game cleared text"
+          style={{
+            height: '150px',
+            position: 'absolute',
+            left: '350px',
+            top: '300px',
+          }}
+        />
+        <img
+          src="/end-page/lucas-face.png"
+          alt="lucas' face"
+          style={{
+            height: '150px',
+            position: 'absolute',
+            left: '800px',
+            top: '500px',
+          }}
+        />
+        <button
+          onClick={handleGoCongrats}
+          style={{
+            position: 'absolute',
+            left: '820px',
+            top: '700px',
+          }}
+        >
+          go to congrats
+        </button>
       </div>
     </>
   )
