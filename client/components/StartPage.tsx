fae73c4cb2f4fa38b44da362a5346921dd9e4077
Haruka Ogino <harukariq@gmail.com>
2024-04-15 16:47:09 +1200

    component steup

---
+++ client/components/StartPage.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function StartPage() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    document.body.style.backgroundImage = "url('/start-game/start-page.png')"
+  })
+
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+  }
+
+  function handleClick() {
+    navigate('/dining-room')
+  }
+
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
+    <img
+      src="/start-game/start-game-thin.png"
+      alt="start game button"
+      style={{
+        height: '100px',
+        position: 'absolute',
+        left: '650px',
+        top: '750px',
+        filter: isHovered
+          ? 'drop-shadow(1px 1px 0  rgb(217, 189, 243)) drop-shadow(-1px -1px 0  rgb(217, 189, 243)) drop-shadow(1px -1px 0  rgb(217, 189, 243)) drop-shadow(-1px 1px 0  rgb(217, 189, 243))'
+          : 'none',
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      onClick={handleClick}
+    />
+  )
+}
