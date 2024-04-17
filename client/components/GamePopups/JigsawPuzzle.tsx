d6233538e8fc13a898befcdd5067633cefdd8df7
Prashniel Kumar <prashniel@gmail.com>
2024-04-17 20:54:04 +1200

    add button variants to reset

--- client/components/GamePopups/JigsawPuzzle.tsx
+++ client/components/GamePopups/JigsawPuzzle.tsx
@@ -258,7 +258,7 @@ export default function JigsawPuzzle({ setWin, win }: Props) {
             width: '160px',
             height: 'auto',
           }}
-          src="/puzzle-images/reset.png"
+          src="/puzzle-images/reset-background.png"
           alt="reset button"
         />
       </button>
