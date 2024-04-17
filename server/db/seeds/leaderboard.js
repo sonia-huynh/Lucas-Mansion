0f0be9d74c443df3c532c153861b80cb8d611d3d
Prashniel Kumar <prashniel@gmail.com>
2024-04-18 08:39:22 +1200

    update seeds

--- server/db/seeds/leaderboard.js
+++ server/db/seeds/leaderboard.js
@@ -4,11 +4,11 @@ export async function seed(knex) {
 
   // Inserts seed entries
   await knex('leaderboard').insert([
-    { id: 1, name: 'LUCAS', time: 55 },
-    { id: 2, name: 'SONIA', time: 85 },
-    { id: 3, name: 'PRASH', time: 94 },
-    { id: 4, name: 'HARUKA', time: 87 },
-    { id: 5, name: 'DARRAGH', time: 90 },
-    { id: 6, name: 'KODA', time: 45 },
+    { id: 1, name: 'LUCAS', time: 1278 },
+    { id: 2, name: 'SONIA', time: 1312 },
+    { id: 3, name: 'PRASH', time: 1245 },
+    { id: 4, name: 'HARUKA', time: 1212 },
+    { id: 5, name: 'DARRAGH', time: 1345 },
+    { id: 6, name: 'KODA', time: 1423 },
   ])
 }
