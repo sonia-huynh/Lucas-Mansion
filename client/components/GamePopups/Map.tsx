df48505d8a6354f26adcb409e336ddd9eedc5915
Sonia <155363911+sonia-huynh@users.noreply.github.com>
2024-04-17 14:31:05 +1200

    audio for inventory and maps complete

--- client/components/GamePopups/Map.tsx
+++ client/components/GamePopups/Map.tsx
@@ -1,23 +1,22 @@
 import React, { useState } from 'react'
-import Puzzle from './Puzzle'
-import JigsawPuzzle from './JigsawPuzzle'
-import JigsawFrame from './JigsawFrame'
+
+//audio:
+import paper from '/audio/paper.mp3'
 
 interface Props {
   setMap: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const Map: React.FC<Props> = ({ setMap }) => {
-
   const [side, setSide] = useState('front')
   const [isHovered, setIsHovered] = useState(false)
 
   function handleSeeBack() {
-    setSide('back')
+    paperSound.play(), setSide('back')
   }
 
   function handleSeeFront() {
-    setSide('front')
+    paperSound.play(), setSide('front')
   }
 
   const handleMouseEnter = () => {
@@ -27,61 +26,62 @@ const Map: React.FC<Props> = ({ setMap }) => {
   const handleMouseLeave = () => {
     setIsHovered(false)
   }
+  const paperSound = new Audio(paper)
 
   return (
     <>
-          <button className="close" onClick={() => setMap(false)}>
-            x
-          </button>
-          <div>
-            { side === 'front' ? (
-              <>
-                <img
-                  src="/puzzle-images/full_map.png"
-                  alt="maze-map"
-                  style={{ height: '500px' }}
-                />
-                {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
-                <img
-                  onClick={handleSeeBack}
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
-                  src="/puzzle-images/flip-over.png"
-                  alt="flip-over-button"
-                  style={{
-                    height: '100px',
-                    filter: isHovered
-                      ? 'drop-shadow(2px 2px 0 white) drop-shadow(-2px -2px 0 white) drop-shadow(2px -2px 0 white) drop-shadow(-2px 2px 0 white)'
-                      : // 'drop-shadow(8px 8px 5px white)'
-                        'none',
-                  } }
-                />
-              </>
-            ) : (
-              <>
-                <img
-                  src="/puzzle-images/back-map.png"
-                  alt="maze-map"
-                  style={{ height: '500px' }}
-                />
-                 {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
-                 <img
-                  onClick={handleSeeFront}
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
-                  src="/puzzle-images/flip-over.png"
-                  alt="flip-over-button"
-                  style={{
-                    height: '100px',
-                    filter: isHovered
-                      ? 'drop-shadow(2px 2px 0 white) drop-shadow(-2px -2px 0 white) drop-shadow(2px -2px 0 white) drop-shadow(-2px 2px 0 white)'
-                      : // 'drop-shadow(8px 8px 5px white)'
-                        'none',
-                  } }
-                  />
-              </>
-            )}
-          </div>
+      <button className="close" onClick={() => setMap(false)}>
+        x
+      </button>
+      <div>
+        {side === 'front' ? (
+          <>
+            <img
+              src="/puzzle-images/full_map.png"
+              alt="maze-map"
+              style={{ height: '500px' }}
+            />
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+            <img
+              onClick={handleSeeBack}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              src="/puzzle-images/flip-over.png"
+              alt="flip-over-button"
+              style={{
+                height: '100px',
+                filter: isHovered
+                  ? 'drop-shadow(2px 2px 0 white) drop-shadow(-2px -2px 0 white) drop-shadow(2px -2px 0 white) drop-shadow(-2px 2px 0 white)'
+                  : // 'drop-shadow(8px 8px 5px white)'
+                    'none',
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <img
+              src="/puzzle-images/back-map.png"
+              alt="maze-map"
+              style={{ height: '500px' }}
+            />
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */}
+            <img
+              onClick={handleSeeFront}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              src="/puzzle-images/flip-over.png"
+              alt="flip-over-button"
+              style={{
+                height: '100px',
+                filter: isHovered
+                  ? 'drop-shadow(2px 2px 0 white) drop-shadow(-2px -2px 0 white) drop-shadow(2px -2px 0 white) drop-shadow(-2px 2px 0 white)'
+                  : // 'drop-shadow(8px 8px 5px white)'
+                    'none',
+              }}
+            />
+          </>
+        )}
+      </div>
     </>
   )
 }
