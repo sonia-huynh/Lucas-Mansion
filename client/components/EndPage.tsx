d257f3064209b7644232a8363d1cd8528fe36c0f
Haruka <155341042+haruka-ogino@users.noreply.github.com>
2024-04-17 14:51:33 +1200

    refactor endPage CSS

--- client/components/EndPage.tsx
+++ client/components/EndPage.tsx
@@ -11,22 +11,12 @@ export default function EndPage({ timer }: Props) {
   const [leaderboard, setLeaderboard] = useState(false)
   const [block, setBlock] = useState(false)
   const [score, setScore] = useState({ name: '', time: '' })
-  const [submition, setSubmition] = useState(false)
+  const [submission, setSubmission] = useState(false)
   const mutation = useLeaderboardMutation()
 
   useEffect(() => {
     document.body.style.backgroundImage = "url('/end-page/end-page.png')"
   })
-
-  const [isHovered, setIsHovered] = useState(false)
-
-  const handleMouseEnter = () => {
-    setIsHovered(true)
-  }
-
-  const handleMouseLeave = () => {
-    setIsHovered(false)
-  }
 
   function handleOpenLeaderboard() {
     setLeaderboard(true)
@@ -37,7 +27,7 @@ export default function EndPage({ timer }: Props) {
     e.preventDefault()
     mutation.mutate(score)
     setScore({ name: '', time: '' })
-    setSubmition(true)
+    setSubmission(true)
   }
 
   return (
@@ -49,58 +39,44 @@ export default function EndPage({ timer }: Props) {
           className="background-img"
         />
         <div className="message">
-          <img
-            src="/end-page/lucas-face.png"
-            alt="lucas' face"
-            style={{
-              height: '170px',
-            }}
-          />
-          <div className="your-score">
-            <h1 id="score-title">Your score:</h1>
-            <p
-              style={{
-                marginBlockStart: '0px',
-                marginBlockEnd: '0px',
-                filter:
-                  'drop-shadow(1px 1px 0px wheat) drop-shadow(-1px -1px 0px wheat) drop-shadow(1px -1px 0px wheat) drop-shadow(-1px 1px 0px wheat)',
-              }}
-            >
-              {`${Math.floor(timer / 60)} minutes and ${timer % 60} seconds.`}
-            </p>
+          <img src="/end-page/lucas-face.png" alt="lucas' face" />
+          {timer > 0 ? (
+            <div className="your-score">
+              <h1 className="score-title">Your score:</h1>
+              <p>
+                {`${Math.floor(timer / 60)} minutes and ${timer % 60} seconds.`}
+              </p>
 
-            {!submition && (
-              <>
-                <p
-                  style={{
-                    filter:
-                      'drop-shadow(1px 1px 0px wheat) drop-shadow(-1px -1px 0px wheat) drop-shadow(1px -1px 0px wheat) drop-shadow(-1px 1px 0px wheat)',
-                  }}
-                >
-                  Add your score to the leaderboard.
-                </p>
-                <form onSubmit={handleSubmit}>
-                  <input
-                    required
-                    onChange={(e) =>
-                      setScore({
-                        name: e.target.value.toUpperCase(),
-                        time: String(timer),
-                      })
-                    }
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={score.name}
-                    placeholder="Your name"
-                  />
-                  <button type="submit" className="submit-name">
-                    submit
-                  </button>
-                </form>
-              </>
-            )}
-          </div>
+              {!submission && (
+                <>
+                  <p>Add your score to the leaderboard.</p>
+                  <form onSubmit={handleSubmit}>
+                    <input
+                      required
+                      onChange={(e) =>
+                        setScore({
+                          name: e.target.value.toUpperCase(),
+                          time: String(timer),
+                        })
+                      }
+                      type="text"
+                      name="name"
+                      id="name"
+                      value={score.name}
+                      placeholder="Your name"
+                    />
+                    <button type="submit" className="submit-name">
+                      submit
+                    </button>
+                  </form>
+                </>
+              )}
+            </div>
+          ) : (
+            <div className="your-score" id="not-cleared">
+              <h1 className="score-title">You have not cleared the game!</h1>
+            </div>
+          )}
         </div>
         {leaderboard && (
           <div className="popup-overlay">
